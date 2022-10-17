24f828beb335ef6540cb28e14c1d8ddde04201e6
alisanaydenysheva <93647369+alisanaydenysheva@users.noreply.github.com>
2022-10-17 11:13:26 +0300

    Create AdminFrame.xaml.cs

---
+++ Демоэкзамен/Глазки-save/WPF - глазки-save/Frames/AdminFrame.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace WPF___глазки_save.frame
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : Page
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+    }
+}
