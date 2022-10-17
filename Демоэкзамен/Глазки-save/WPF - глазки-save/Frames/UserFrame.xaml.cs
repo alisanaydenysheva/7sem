d08a25ca838e9094901a7b27efbc9d6d27a091c1
alisanaydenysheva <93647369+alisanaydenysheva@users.noreply.github.com>
2022-10-17 11:12:48 +0300

    Create UserFrame.xaml.cs

---
+++ Демоэкзамен/Глазки-save/WPF - глазки-save/Frames/UserFrame.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace WPF___глазки_save.frame
+{
+    /// <summary>
+    /// Interaction logic for User.xaml
+    /// </summary>
+    public partial class User : Page
+    {
+        public User()
+        {
+            InitializeComponent();
+        }
+    }
+}
