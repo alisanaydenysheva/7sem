a4eb66df591926f1e3cab3ec8298a6d0e0b894dc
alisanaydenysheva <93647369+alisanaydenysheva@users.noreply.github.com>
2022-10-19 07:52:35 +0300

    Create ShiftSupervisorPage.xaml.cs

---
+++ Демоэкзамен/Благодать/Готовое/WPF - Благодать/Pages/ShiftSupervisor/ShiftSupervisorPage.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF___Благодать.Pages.ShiftSupervisor
+{
+    /// <summary>
+    /// Interaction logic for ShiftSupervisorPage.xaml
+    /// </summary>
+    public partial class ShiftSupervisorPage : Page
+    {
+        public ShiftSupervisorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
